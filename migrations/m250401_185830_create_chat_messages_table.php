f5ea05c16e81ff591bc8d1038747c5b9c71e4b9f
Anton Lykinov <antonlykinov96@gmail.com>
2025-04-04 10:19:40 +0300

    fix

--- migrations/m250401_185830_create_chat_messages_table.php
+++ migrations/m250401_185830_create_chat_messages_table.php
@@ -2,6 +2,7 @@
 
 use yii\db\Migration;
 
+
 /**
  * Handles the creation of table `{{%chat_messages}}`.
  */
@@ -9,6 +10,28 @@ class m250401_185830_create_chat_messages_table extends Migration
 {
     public function up()
     {
+        $dbName = 'chat_db';
+        $db = Yii::$app->db;
+        
+
+        $schemaExists = $db->createCommand("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = :dbName", [
+            ':dbName' => $dbName
+        ])->queryScalar();
+        
+        if (!$schemaExists) {
+            $db->createCommand("CREATE DATABASE `{$dbName}` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")->execute();
+            Yii::$app->session->setFlash('success', "Database {$dbName} created successfully.");
+        }
+        
+
+        $this->db = new \yii\db\Connection([
+            'dsn' => 'mysql:host=localhost;dbname=' . $dbName,
+            'username' => Yii::$app->db->username,
+            'password' => Yii::$app->db->password,
+            'charset' => 'utf8mb4',
+        ]);
+        
+
         $this->createTable('chat_messages', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull(),
@@ -19,6 +42,18 @@ class m250401_185830_create_chat_messages_table extends Migration
 
     public function down()
     {
+
+        $this->db = new \yii\db\Connection([
+            'dsn' => 'mysql:host=localhost;dbname=chat_db',
+            'username' => Yii::$app->db->username,
+            'password' => Yii::$app->db->password,
+            'charset' => 'utf8mb4',
+        ]);
+        
         $this->dropTable('chat_messages');
+        
+        // Опционально: удаляем базу данных (если нужно)
+        // $db = Yii::$app->db;
+        // $db->createCommand("DROP DATABASE IF EXISTS `chat_db`")->execute();
     }
 }
